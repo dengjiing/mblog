a96a8e4dafa12dcad6828e064d917e5b2c4948fc
dengjiing <dengjiing96@gmail.com>
2018-03-24 21:40:49 +0800

    添加了文章内容

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Post
+from django.http import HttpResponse
+
+
+#Create your views here.
+def homepage(request):
+	posts = Post.objects.all()
+	post_lists = list()
+	for count,post in enumerate(posts):
+		post_lists.append("NO.{}:".format(str(count))+str(post.title)+"<br>")
+		post_lists.append("<small>"+str(post.body)+"</small><br><br>")
+	return HttpResponse(post_lists)
+
+
+#
+# from django.template.loader import get_template
+# from gjango.http import  HttpResponse
+# from datetime import datetime
+# from .models import Post
+#
+# #create your views here.
+# def homepage(request):
+# 	template = get_template('index.html')
+# 	posts = Post.objects.all()
+# 	now = datetime\
+# 	ht	.render(locals())
--- mblog/urls.py
+++ mblog/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include,url
+from mainsite.views import homepage
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'admin/', admin.site.urls),
+    url(r'^$',homepage),
+	#url(r'^admin/',include(admin.site.urls)),
 ]
